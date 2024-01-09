4135339dd360ec120bb5f1775cc2ceea34d0dd1c
Muhammad Zariyan Asif <33691797+MuhammadZariyanAsif123@users.noreply.github.com>
2024-01-09 20:30:26 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TipCalculater from './components/TipCalculater';
+
+function App() {
+  return (
+    <div>
+    <TipCalculater/>
+    </div>
+    
+  );
+}
+
+export default App;
